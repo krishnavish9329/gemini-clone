572b4ccbdf05b898c719f67b719242d18caff007
krishnavish9329 <Krishna.vish9329@gmail.com>
2025-04-25 08:27:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Sidebar from './component/Sodebar/Sidebar'
+import Main from "./component/Main/Main"
+
+function App() {
+
+  return (
+    <>
+    <Sidebar />
+    <Main/>
+    
+    </>
+  )
+}
+
+export default App
